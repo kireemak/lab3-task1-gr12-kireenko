d3b4c44967286bac03801961e5c87a549bad9efe
Makar Kireenko <137899428+kireemak@users.noreply.github.com>
2024-03-13 20:50:32 +0000

    Add removeEvenPositionLetters function to main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int removeEvenPositionLetters(char* sentence);
+int removeEvenPositionLetters(char *sentence) {
+    int removedCount = 0;
+    int length = strlen(sentence);
+
+    for (int i = 1; i < length; i += 2) {
+        sentence[i / 2] = sentence[i];
+        removedCount++;
+    }
+
+    sentence[length / 2] = '\0';
+
+    return removedCount;
+}
 
 int main() {
     char sentence[300];
